81a9f33d8b0af34bac405d2cd2ca0bbf915e1a16
unknown <maiderc@opendeusto.es>
2019-10-24 12:04:59 +0200

    Interface for user case 'Inicio sesión' done (first stage)

---
+++ src/Controller/clsController.java
@@ -0,0 +1,20 @@
+package Controller;
+import Swing.*;
+
+public class clsController
+{
+    //private clsServiceLocator sl = null;
+
+    public clsController() //throws RemoteException
+    {
+        //sl = new clsServiceLocator();
+        //sl.setService();
+
+        //Habra que poner la ventana principal cuando este lista
+        //frmInicio GUI = new frmInicio(this);
+        frmInicioSesion GUI = new frmInicioSesion(this);
+        GUI.setVisible(true);
+
+    }
+
+}
